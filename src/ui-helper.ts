30b40d4b9ad6fc36c658b92c55888801da4cdc83
Adrian <Adrian>
2021-02-13 22:54:53 +0100

    UI

--- src/ui-helper.ts
+++ src/ui-helper.ts
@@ -1,12 +1,12 @@
 export function toInt(num: string): number {
-    const int = parseInt(num);
+    const int = parseInt(num, 10);
     if (Number.isNaN(int)) {
-        error('"' + num + "'' is not a number!");
+        error('"' + num + "'' ist keine Zahl!");
     }
     return int;
 }
 
-export function error(message: string) {
+export function error(message: string): never {
     alert(message);
     throw Error(message);
 }
@@ -14,4 +14,8 @@ export function error(message: string) {
 export function format(num: number): string {
     console.log(num);
     return Math.round(num * 10) / 10 + '';
+}
+
+export function isAlmostZero(value: number): boolean {
+    return value < 0.000001 && value > -0.000001;
 }
--- src/ui.ts
+++ src/ui.ts
@@ -1,36 +1,103 @@
-import * as $ from 'jquery';
 import { calculate, EanResult, EanSource, EanTank } from "./calcularor";
-import { format, toInt } from './ui-helper';
+import { BlendResult, calculateForRemosEquiptment, COMPRESSOR_OUTPUT_200_BAR_STOPS_AT, DEFAULT_SOURCE_OXYGEN_PERCENTAGE, displayToRealPressureBar } from './remo-calculator';
+import { error, format, isAlmostZero, toInt } from './ui-helper';
 
-$('.calc').click(() => {
-	const myTankBefore: EanTank = {
-  	pressureBar: toInt($('.tankBeforePressure').val() as string),
-   	oxygenPercentage: toInt($('.tankBeforOxygen').val() as string)
-  }
-  const sourceTank: EanSource = {
-    oxygenPercentage: toInt($('.sourceOxygen').val() as string)
-	}
+export function init() {
+  setDefaults();
+  registerListerners();
+}
 
-	const myTankAfter: EanTank = {
-  	pressureBar: toInt($('.tankAfterPressure').val() as string),
-   	oxygenPercentage: toInt($('.tankAfterOxygen').val() as string)
+function updateTankPressure() {
+  const pressureBarDisplayed = getInputValue('.tankPressureBeforeDesiplayed');
+  const pressureBarDisplayedAsNumber = parseInt(pressureBarDisplayed);
+  const pressureBar = displayToRealPressureBar(pressureBarDisplayedAsNumber);
+  if (Number.isFinite(pressureBarDisplayedAsNumber) && pressureBar >= 0) {
+    setInputValue('.tankPressureBefore', format(pressureBar));
+  } else {
+    setInputValue('.tankPressureBefore', '');
   }
-  
-  const result: EanResult = calculate(myTankBefore, myTankAfter, sourceTank);
-  
-  const addBooster = 'add +' 
-  	+ format(result.addBarEanSource) 
-    + 'bar to ' 
-    + format(result.addBarEanSourceToTarget) 
-    + 'bar of EAN' 
-    + format(sourceTank.oxygenPercentage);
-  const addAir = 'add +'
-  	+ format(result.addBarAir)
-    + 'bar to '
-    + format(result.addBarAirToTarget) 
-    + 'bar of air';
-  
-  $('.addFromSource').val(addBooster);
-  $('.addAir').val(addAir);
-  
-});
+}
+
+function doCalculation() {
+  const result: BlendResult = calculateForRemosEquiptment({
+    currentPressureTankBarDisplayed: getInputValueParsed('.tankPressureBeforeDesiplayed'),
+    currentOxygenPercentage: getInputValueParsed('.tankOxygenBefore'),
+
+    targetTankPressureBar: getInputValueParsed('.tankPressureTarget'),
+    targetTankOxygenPercentage: getInputValueParsed('.tankOxygenTarget'),
+
+    sourceTankOgygenPercentage: getInputValueParsed('.sourceOxygen')
+  });
+
+  displayResultDeleyed(result);
+}
+
+function displayResultDeleyed(result: BlendResult) {
+  displayResult({
+    releaseBar: 0,
+    releaseBarTo: 0,
+    releaseBarToDisplayBooster: 0,
+    addBarSource: 0,
+    addBarSourceToDisplayBooster: 0,
+    addBarAir: 0,
+    addBarAirTo: 0,
+    addBarAirToDisplayCompressor: 0
+  });
+  setTimeout(() => {
+    displayResult(result);
+  }, 300);
+}
+
+function displayResult(result: BlendResult) {
+  const needToReleseGas = !isAlmostZero(result.releaseBar);
+  setVisibility('.release', needToReleseGas);
+  setInputValue('.releaseFromTank', format(result.releaseBar));
+  setInputValue('.releaseFromTankTo', format(result.releaseBarTo));
+
+  const needToBoost = !isAlmostZero(result.addBarSource);
+  setVisibility('.booster', needToBoost);
+  setInputValue('.addFromSource', format(result.addBarSource));
+  setInputValue('.addFromSourceTo', format(result.addBarSourceToDisplayBooster));
+
+  const needCompressor = !isAlmostZero(result.addBarAir);
+  setVisibility('.compressor', needCompressor);
+  setInputValue('.addAir', format(result.addBarAir));
+  setInputValue('.addAirTo', format(result.addBarAirToDisplayCompressor));
+
+}
+
+function registerListerners() {
+  addListener('.calc', 'click', doCalculation);
+  addListener('.tankPressureBeforeDesiplayed', 'input', updateTankPressure)
+}
+
+function setDefaults() {
+  setInputValue('.sourceOxygen', format(DEFAULT_SOURCE_OXYGEN_PERCENTAGE));
+  setInputValue('.tankPressureTarget', format(COMPRESSOR_OUTPUT_200_BAR_STOPS_AT));
+
+}
+
+function getInputValueParsed(selector: string): number {
+  const value = getInputValue(selector);
+  const valueAsNumber = toInt(value);
+  if (valueAsNumber < 0) {
+    error(`'${valueAsNumber}' ist kleiner 0!`);
+  }
+  return valueAsNumber;
+}
+function getInputValue(selector: string): string {
+  return (document.querySelector(selector) as HTMLInputElement).value;
+}
+
+function setInputValue(selector: string, value: string): void {
+  (document.querySelector(selector) as HTMLInputElement).value = value;
+}
+
+function addListener(selector: string, eventType: string, action: () => void) {
+  document.querySelector(selector).addEventListener(eventType, action);
+}
+
+function setVisibility(selector: string, visible: boolean) {
+  const visibility = visible ? 'inherit' : 'hidden'
+  document.querySelectorAll(selector).forEach( (element: HTMLElement) => element.style.visibility = visibility)
+}
